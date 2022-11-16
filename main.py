bbb73ef4a493b72f37ec9de6aae3717ceebae9fc
Ahmad1Jaber <ahmadjaber1998@gmail.com>
2022-11-17 02:19:46 +0300

    Airport

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 from airline import *
 from aircraft import *
+from airport import *
 
 print("Welcome to Airline Search")
 getAirline = input("Enter Airline Code: ")
 airlineInfo(getAirline)
 getAircraft = input("Enter Aircraft Code: ")
 aircraftInfo(getAircraft)
+getAirport = input("Enter Airport Code: ")
+airportInfo(getAirport)
